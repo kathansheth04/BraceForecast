22ea9816fd9f1dd3e5801b647c78cdace09e5597
kathansheth04 <sheth.kathan04@gmail.com>
2020-11-14 15:02:01 -0800

    login

--- lib/Screens/Landing/background.dart
+++ lib/Screens/Landing/background.dart
@@ -19,18 +19,14 @@ class Background extends StatelessWidget {
           Positioned(
             top: 0,
             left: -10,
-            child: Image.asset(
-              "assets/images/main_top.png",
-              width: size.width * 1
-            ),
+            child: Image.asset("assets/images/main_top.png",
+                width: size.width * 1),
           ),
           Positioned(
             top: 0,
             right: 0,
-            child: Image.asset(
-              "assets/images/main_topright.png",
-              width: size.width * 0.9
-            ),
+            child: Image.asset("assets/images/main_topright.png",
+                width: size.width * 0.9),
           ),
           Positioned(
             bottom: 100,
--- lib/Screens/Landing/body.dart
+++ lib/Screens/Landing/body.dart
@@ -14,37 +14,35 @@ class Body extends StatelessWidget {
             SizedBox(height: size.height * 0.5),
             SizedBox(height: size.height * 0.05),
             new Container(
-                margin: EdgeInsets.only(top: 5),
-                padding: EdgeInsets.all(10.0),
-                width: size.width * 0.85, 
-                child: RaisedButton.icon(
-                    onPressed: () {},
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                    label: Text('Log in',
-                        style: TextStyle(color: Colors.black, fontSize: 17)),
-                    icon:
-                        Icon(Icons.supervised_user_circle, color: Colors.black),
-                    padding: const EdgeInsets.all(13.0),
-                    splashColor: Colors.lightBlue[100],
-                    color: Colors.lightBlue[200]),
-              ),
+              margin: EdgeInsets.only(top: 5),
+              padding: EdgeInsets.all(10.0),
+              width: size.width * 0.85,
+              child: RaisedButton.icon(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                  label: Text('Log in',
+                      style: TextStyle(color: Colors.black, fontSize: 17)),
+                  icon: Icon(Icons.supervised_user_circle, color: Colors.black),
+                  padding: const EdgeInsets.all(13.0),
+                  splashColor: Colors.lightBlue[100],
+                  color: Colors.lightBlue[200]),
+            ),
             new Container(
-                margin: EdgeInsets.only(top: 5),
-                padding: EdgeInsets.all(10.0),
-                width: size.width * 0.85, 
-                child: RaisedButton.icon(
-                    onPressed: () {},
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                    label: Text('Register',
-                        style: TextStyle(color: Colors.black, fontSize: 17)),
-                    icon:
-                        Icon(Icons.supervised_user_circle, color: Colors.black),
-                    padding: const EdgeInsets.all(13.0),
-                    splashColor: Colors.lightBlue[200],
-                    color: Colors.lightBlue[100]),
-              ),
+              margin: EdgeInsets.only(top: 5),
+              padding: EdgeInsets.all(10.0),
+              width: size.width * 0.85,
+              child: RaisedButton.icon(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                  label: Text('Register',
+                      style: TextStyle(color: Colors.black, fontSize: 17)),
+                  icon: Icon(Icons.supervised_user_circle, color: Colors.black),
+                  padding: const EdgeInsets.all(13.0),
+                  splashColor: Colors.lightBlue[200],
+                  color: Colors.lightBlue[100]),
+            ),
           ],
         ),
       ),
